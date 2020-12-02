dd8808e936bff86d1406e707284de9dfb61d37f6
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-02 19:25:13 -0300

    feat(app): add Navigation and ToastContainer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
+import { ToastContainer } from 'react-toastify';
 
 import apolloClient from 'lib/apolloClient';
+import Navigation from 'components/Navigation';
 
 import GlobalStyles from 'styles/global';
 
@@ -10,7 +13,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
       <Head>
-        <title>NextJS CookBook</title>
+        <title>NextJS Cookbook</title>
         <link rel="shortcut icon" href="/images/icon-512.png" />
         <link rel="apple-touch icon" href="/images/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -22,6 +25,20 @@ function App({ Component, pageProps }: AppProps) {
 
       <GlobalStyles />
 
+      <Navigation />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        draggable={false}
+        css={{
+          position: 'fixed',
+          bottom: '24px',
+          right: '24px',
+          width: '586px',
+        }}
+      />
       <Component {...pageProps} />
     </ApolloProvider>
   );
