a0aa0ce7b552a48435db10d7f9c2597767cedac1
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-02 17:11:08 -0300

    feat: create Loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Loader() {
+  return <S.Loader />;
+}
+
+export default Loader;
---
+++ src/components/Loader/styles.js
@@ -0,0 +1,37 @@
+import styled, { keyframes } from 'styled-components';
+
+import colors from 'styles/variables/colors';
+
+const loaderSize = '25px';
+const borderSize = '5px';
+
+export const Loader = styled.div`
+  width: ${loaderSize} + ${borderSize} * 2;
+  height: ${loaderSize} + ${borderSize} * 2;
+  position: relative;
+  align-self: center;
+  margin-left: 10px;
+  animation: ${spin} 0.5s infinite linear;
+
+  &::after {
+    content: '';
+    width: ${loaderSize};
+    height: ${loaderSize};
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: ${borderSize} solid transparent;
+    border-top-color: ${colors.dark_gray};
+    border-radius: 100%;
+  }
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0);
+  }
+  
+  100% {
+    transform: rotate(360deg);
+  }
+`;
