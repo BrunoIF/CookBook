b1fd2ce30307bee5d32a5d65bfaee98ea313aa36
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-08 17:36:25 -0300

    refactor: change _app to .jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
 import { ToastContainer } from 'react-toastify';
 
 import { AuthProvider, ProtectRoute } from 'context/auth';
-import apolloClient from 'lib/apolloClient';
+import { useApollo } from 'lib/apolloClient';
 import Navigation from 'components/Navigation';
 
 import GlobalStyles from 'styles/global';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <ApolloProvider client={apolloClient}>
+    <>
       <Head>
         <title>NextJS Cookbook</title>
         <link rel="shortcut icon" href="/images/icon-512.png" />
@@ -26,26 +27,28 @@ function App({ Component, pageProps }: AppProps) {
 
       <GlobalStyles />
 
-      <AuthProvider>
-        <Navigation />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          closeOnClick
-          draggable={false}
-          css={{
-            position: 'fixed',
-            bottom: '24px',
-            right: '24px',
-            width: '586px',
-          }}
-        />
-        <ProtectRoute>
-          <Component {...pageProps} />
-        </ProtectRoute>
-      </AuthProvider>
-    </ApolloProvider>
+      <ApolloProvider client={apolloClient}>
+        <AuthProvider>
+          <Navigation />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            closeOnClick
+            draggable={false}
+            css={{
+              position: 'fixed',
+              bottom: '24px',
+              right: '24px',
+              width: '586px',
+            }}
+          />
+          <ProtectRoute>
+            <Component {...pageProps} />
+          </ProtectRoute>
+        </AuthProvider>
+      </ApolloProvider>
+    </>
   );
 }
 
