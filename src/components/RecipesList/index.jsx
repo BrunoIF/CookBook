24bdc6b447c15b20be501f8c065e61dacb4eb6bc
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-09 15:18:09 -0300

    refactor: update RecipesList initial query

--- src/components/RecipesList/index.jsx
+++ src/components/RecipesList/index.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { useSubscription } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
 
-import { GET_ALL_RECIPE_CREATED } from 'queries/recipes';
+import { GET_ALL_RECIPES } from 'queries/recipes';
 import Card from 'components/Card';
 
 import * as S from 'styles/index.styles';
 import { Title } from 'styles/global';
 
 function RecipesList() {
-  const { data, error, loading } = useSubscription(GET_ALL_RECIPE_CREATED);
+  const { data: initialRecipes, error, loading } = useQuery(GET_ALL_RECIPES);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    if (initialRecipes) {
+      const { recipes } = initialRecipes;
+      setRecipes((prevState) => [...prevState, ...recipes]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialRecipes]);
 
   if (error) return <h1>Error loading recipes</h1>;
   if (loading) return <h1>Loading...</h1>;
-
-  const { recipes } = data;
 
   return (
     <div>
