0752651b4efea24fe1851e83891abc8432189672
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-10 09:24:58 -0300

    styles: remove comment

--- src/styles/variables/colors.js
+++ src/styles/variables/colors.js
@@ -1,7 +1,6 @@
 const colors = {
   white: 'hsl(0, 0%, 100%)',
   black: '#000',
-  // Light Theme
   dark_gray: 'hsl(0, 0%, 52%)',
   background_light: 'hsl(0, 0%, 98%)',
   dark_blue: 'hsl(200, 15%, 8%)',
