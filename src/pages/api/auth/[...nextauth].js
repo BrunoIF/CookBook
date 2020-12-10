beae64d41eb6add7c146767717bb11eb12106420
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-10 10:57:46 -0300

    feat: create next-auth api route

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { initializeApollo } from 'lib/apolloClient';
+import { GET_AUTH_TOKEN } from 'queries/authentication';
+
+const options = {
+  providers: [
+    Providers.Credentials({
+      name: 'Credentials',
+
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        const apolloClient = initializeApollo();
+
+        const { username, password } = credentials;
+
+        return apolloClient
+          .mutate({
+            mutation: GET_AUTH_TOKEN,
+            variables: { username, password },
+          })
+          .then((result) => {
+            if (result.data) {
+              const { data } = result;
+              const user = {
+                ...data.payload,
+                token: data.token,
+                refreshExpiresIn: data.refreshExpiresIn,
+              };
+              return Promise.resolve(user);
+            } else {
+              return Promise.resolve(null);
+            }
+          })
+          .catch((err) => Promise.reject(err));
+      },
+    }),
+  ],
+};
+
+export default (req, res) => NextAuth(req, res, options);
