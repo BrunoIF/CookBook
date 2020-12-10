6f5ba372f9399082508c020393631e224e880a85
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-10 16:08:25 -0300

    refactor: update Login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { Wrapper as GlobalWrapper, Title, Text } from 'styles/global';
+import { signIn } from 'next-auth/client';
+
+import { Wrapper as GlobalWrapper } from 'styles/global';
 import Button from 'components/Buttons/Button';
-import Input from 'components/Input';
-import { useAuth } from 'context/auth';
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
-  const { isAuthenticated, login } = useAuth();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      router.push('/');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated]);
-
-  const handleClick = () => {
-    login(username, password);
-  };
-
   return (
     <>
       <Head>
@@ -30,17 +13,9 @@ function Login() {
       </Head>
 
       <GlobalWrapper>
-        <Title>Login</Title>
-        <Text>Username:</Text>
-        <Input value={username} onChange={(e) => setUsername(e.target.value)} />
-        <Text>Password:</Text>
-        <Input value={password} onChange={(e) => setPassword(e.target.value)} />
-        <Button
-          css={{ marginTop: '30px' }}
-          text="Login"
-          disabled={!username || !password}
-          onClick={handleClick}
-        />
+        <div css={{ display: 'grid', placeItems: 'center' }}>
+          <Button css={{ marginTop: '30px' }} text="Login" onClick={signIn} />
+        </div>
       </GlobalWrapper>
     </>
   );
