a9f95909bc2cf1ac87a9af9e289942a452b19cae
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-11 11:32:10 -0300

    feat: add redirect on Login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
-import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { signIn, useSession } from 'next-auth/client';
 
 import { Wrapper as GlobalWrapper } from 'styles/global';
 import Button from 'components/Buttons/Button';
 
 function Login() {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) router.push('/');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading]);
+
   return (
     <>
       <Head>
