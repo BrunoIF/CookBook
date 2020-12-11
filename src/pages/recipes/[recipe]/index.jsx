61cb53e776ceed2dd963781436f37118d8349647
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-12-11 14:11:25 -0300

    feat: add ProtectedRoute to all protected pages

--- src/pages/recipes/[recipe]/index.jsx
+++ src/pages/recipes/[recipe]/index.jsx
@@ -9,6 +9,7 @@ import { GET_RECIPE } from 'queries/recipes';
 import Info from 'components/Info';
 import LinkButton from 'components/Buttons/LinkButton';
 import Button from 'components/Buttons/Button';
+import ProtectedRoute from 'components/ProtectedRoute';
 import { Wrapper as GlobalWrapper, Title } from 'styles/global';
 import { initializeApollo, addApolloState } from 'lib/apolloClient';
 
@@ -49,35 +50,41 @@ function Recipe({ recipeId }) {
       <Head>
         <title>{recipe.title}</title>
       </Head>
-      <GlobalWrapper>
-        <LinkButton to="/" text="Back" Icon={backArrow} />
-        {recipe ? (
-          <S.FlexRow css={{ marginTop: '30px' }}>
-            <S.Image src={recipe.image} alt={`${recipe.title}`} />
-            <div css={{ marginLeft: '30px', width: '50%' }}>
-              <Title>{recipe.title}</Title>
-              <S.ContentWrapper>
-                <div>
-                  <Info title="Cooking Time" description={recipe.cookingTime} />
-                </div>
-              </S.ContentWrapper>
-              <S.IngredientsWrapper>
-                <Info title="Ingredients" description={ingredientsList} />
-              </S.IngredientsWrapper>
-              <S.FlexRow css={{ marginTop: '30px' }}>
-                <Button text="Delete Recipe" onClick={handleDeleteRecipe} />
-                <LinkButton
-                  to={`/recipes/${recipeId}/edit`}
-                  css={{ marginLeft: '30px', marginTop: 0 }}
-                  text="Edit Recipe"
-                />
-              </S.FlexRow>
-            </div>
-          </S.FlexRow>
-        ) : (
-          <Title>Not found</Title>
-        )}
-      </GlobalWrapper>
+
+      <ProtectedRoute>
+        <GlobalWrapper>
+          <LinkButton to="/" text="Back" Icon={backArrow} />
+          {recipe ? (
+            <S.FlexRow css={{ marginTop: '30px' }}>
+              <S.Image src={recipe.image} alt={`${recipe.title}`} />
+              <div css={{ marginLeft: '30px', width: '50%' }}>
+                <Title>{recipe.title}</Title>
+                <S.ContentWrapper>
+                  <div>
+                    <Info
+                      title="Cooking Time"
+                      description={recipe.cookingTime}
+                    />
+                  </div>
+                </S.ContentWrapper>
+                <S.IngredientsWrapper>
+                  <Info title="Ingredients" description={ingredientsList} />
+                </S.IngredientsWrapper>
+                <S.FlexRow css={{ marginTop: '30px' }}>
+                  <Button text="Delete Recipe" onClick={handleDeleteRecipe} />
+                  <LinkButton
+                    to={`/recipes/${recipeId}/edit`}
+                    css={{ marginLeft: '30px', marginTop: 0 }}
+                    text="Edit Recipe"
+                  />
+                </S.FlexRow>
+              </div>
+            </S.FlexRow>
+          ) : (
+            <Title>Not found</Title>
+          )}
+        </GlobalWrapper>
+      </ProtectedRoute>
     </>
   );
 }
